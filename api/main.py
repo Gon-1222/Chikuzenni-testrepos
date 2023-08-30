4bb4cf21fa6bc2961bcd609f509ca48f74122979
Gon-1222 <caramelpopsoft@gmail.com>
2023-08-30 22:22:15 +0900

    First version

---
+++ api/main.py
@@ -0,0 +1,40 @@
+from flask import Flask,request,jsonify
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello():
+    name = "Hello World"
+    return "This domain is intended for testing Chikuzen-ni IoT endpoints."
+
+@app.route('/IoT',methods=['POST'])
+def iot():
+    headers= request.headers.get("Chikuzenni-token",None)
+    json=request.get_json()
+    if json == None:
+        if request.headers['Content-Type'] != 'application/json':
+            return_data={"success":False,"message":"Kindly use the Content-Type as application/json."}
+        else:
+            return_data={"success":False,"message":"We can only process JSON data."}
+        return jsonify(return_data)
+    data1 = json.get('token',None)
+    data2 = json.get('status',None)
+    print(data2==None)
+
+    if (headers==None or data1==None or data2==None):
+        return_data={"success":False,"message":"The required information is incomplete."}
+    else:
+        return_data = {
+            "success":True,
+            "data":{
+            "header-token":headers,
+            "body-token":data1,
+            "body-status":data2
+            }
+            }
+    print(return_data)
+    return jsonify(return_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
